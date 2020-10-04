48e552e4b75d4f4b25eef87132803396ab653dfa
Márton Nagy <marton.nagy2@stud.uni-corvinus.hu>
2020-10-04 11:51:30 +0200

    negyzetmeter ar

--- Excel/Excel/Form1.cs
+++ Excel/Excel/Form1.cs
@@ -69,7 +69,7 @@ namespace Excel
                 values[counter, 5] = f.NumberOfRooms;
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
-                values[counter, 8] = "";
+                values[counter, 8] = (f.Price/f.FloorArea)*1000000;
                 counter++;
                 xlSheet.get_Range(
             GetCell(2, 1),
