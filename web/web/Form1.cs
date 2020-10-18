96b53ee2be6c2782b0b162b95abae93d28de78da
Márton Nagy <marton.nagy2@stud.uni-corvinus.hu>
2020-10-18 18:41:02 +0200

    xml

--- web/web/Form1.cs
+++ web/web/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using web.Entities;
 using web.MnbServiceReference;
 
@@ -14,9 +15,12 @@ namespace web
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> Rates;
+         BindingList<RateData> Rates;
+       
+
         private void Currency()
         {
+            Rates = new BindingList<RateData>();
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
@@ -26,11 +30,30 @@ namespace web
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+               
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+                Rates.Add(rate);
+
+            }
         }
+        
+        
         public Form1()
         {
             InitializeComponent();
             Currency();
+           
             dataGridView1.DataSource = Rates.ToList();
         }
     }
