a68013288aaa86678ddc33b5404ecf6e3a478ed0
Márton Nagy <marton.nagy2@stud.uni-corvinus.hu>
2020-11-15 14:11:32 +0100

    termekvalaszto

--- mikulas/mikulas/Form1.cs
+++ mikulas/mikulas/Form1.cs
@@ -14,12 +14,15 @@ namespace mikulas
 {
     public partial class Form1 : Form
     {
+        private Toy _nextToy;
         private List<Toy> _toys = new List<Toy>();
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -52,6 +55,25 @@ namespace mikulas
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
