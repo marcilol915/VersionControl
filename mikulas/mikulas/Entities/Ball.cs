1e2841e71886b9b61a5e54999497539c15be2dc8
Márton Nagy <marton.nagy2@stud.uni-corvinus.hu>
2020-11-15 13:50:00 +0100

    implement

--- mikulas/mikulas/Entities/Ball.cs
+++ mikulas/mikulas/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mikulas.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,28 +9,21 @@ using System.Windows.Forms;
 
 namespace mikulas.Entities
 {
-     public class Ball: Label
+     public class Ball : Toy
     {
         public Ball()
         {
-            AutoSize = false;
-            Width = 50;
-            Height = 50;
-            Paint += Ball_Paint;
+           
 
         }
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-        protected void DrawImage(Graphics g)
+        
+       
+       
+
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-        public void MoveBall()
-        {
-            Left += 1;
         }
     }
 }
