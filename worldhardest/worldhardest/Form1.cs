1177dca8a2408c7d68851fada905cb93b4542175
Márton Nagy <marton.nagy2@stud.uni-corvinus.hu>
2020-11-29 18:03:12 +0100

    100player

--- worldhardest/worldhardest/Form1.cs
+++ worldhardest/worldhardest/Form1.cs
@@ -15,13 +15,23 @@ namespace worldhardest
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-           // gc.AddPlayer();
+            // gc.AddPlayer();
             //gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
         }
     }
 }
